83816729d88140ca6661e2f49af17dd362ae6a70
motherfather <BITUSER@BITPC>
2016-10-14 17:34:06 +0900

    161014 1734

---
+++ WebContent/error/error_404.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>error 404</title>
+</head>
+<body>
+	<h1>요청한 리소스를 찾을 수 없습니다.</h1>
+</body>
+</html>
---
+++ src/com/bit2016/helloweb/servlet/LifeCycleServlet.java
@@ -0,0 +1,38 @@
+package com.bit2016.helloweb.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/lc")
+public class LifeCycleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+		System.out.println("init() called");
+	}
+
+	public void destroy() {
+		System.out.println("destroy() called");
+		super.destroy();
+	}
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("service() called");
+		super.service(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet() called");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
